c53734cf7ab553841b09a42a4983c32c2f2aba20
Samarth Patel <psamarth8836@gmail.com>
2022-03-31 13:03:06 -0400

    string reversal completed

---
+++ src/com/patelsamarth/DsAndAlgo/recursion_string_reverse.java
@@ -0,0 +1,16 @@
+package com.patelsamarth.DsAndAlgo;
+
+public class recursion_string_reverse {
+    public static void main(String[] args) {
+        recursion_string_reverse ref = new recursion_string_reverse();
+        String question = "ABCD";
+        System.out.println(ref.reverseString(question));
+    }
+
+    public String reverseString(String s) {
+        if(s.isEmpty()) {
+            return s;
+        }
+        return reverseString(s.substring(1)) + s.charAt(0);
+    }
+}
