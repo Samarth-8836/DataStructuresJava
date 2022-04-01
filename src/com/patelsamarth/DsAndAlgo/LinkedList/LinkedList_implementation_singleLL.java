7f1490d6e8a0874a2d973cf1c72f6fa62e67ad2e
Samarth Patel <psamarth8836@gmail.com>
2022-03-31 21:47:20 -0400

    Linked List converted to generics

--- src/com/patelsamarth/DsAndAlgo/LinkedList/LinkedList_implementation_singleLL.java
+++ src/com/patelsamarth/DsAndAlgo/LinkedList/LinkedList_implementation_singleLL.java
@@ -3,25 +3,25 @@ package com.patelsamarth.DsAndAlgo.LinkedList;
 public class LinkedList_implementation_singleLL {
     public static void main(String[] args) {
         LinkedList_implementation_singleLL ref = new LinkedList_implementation_singleLL();
-        Single_LL linkedList = ref.new Single_LL();
-        Node headRef = linkedList.createLL(5);
-        System.out.println(headRef.value);
+        Single_LL<String> linkedList = ref.new Single_LL<>();
+        linkedList.createLL("ABC5");
+        System.out.println(linkedList.head.value);
     }
 
-    public class Node {
-        public int value;
-        public Node next;
+    public class Node<T> {
+        public T value;
+        public Node<T> next;
     }
 
-    public class Single_LL {
-        public Node head;
-        public Node tail;
+    public class Single_LL<T> {
+        public Node<T> head;
+        public Node<T> tail;
         public int size;
 
-        public Node createLL(int nodeValue) {
-            head = new Node();
-            tail = new Node();
-            Node node = new Node();
+        public Node<T> createLL(T nodeValue) {
+            head = new Node<T>();
+            tail = new Node<T>();
+            Node<T> node = new Node<T>();
             node.value = nodeValue;
             node.next = null;
             head = node;
